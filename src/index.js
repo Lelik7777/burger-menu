1c83f998891add5fb3422ab84bb8eb92f42cb70d
Lelik Jan <21alex7777@gmail.com>
2023-01-21 15:25:03 +0300

    feat: add in index.js smooth scrolling to sections

--- src/index.js
+++ src/index.js
@@ -28,13 +28,49 @@ const isMobile = {
 }
 if (isMobile.any()) {
     document.body.classList.add('_touch');
+    //механизм анимации стрелки
     //получаем массив всех элементов с классом .menu__arrow
     const arrArrows = Array.from(document.querySelectorAll('.menu__arrow'));
     //при нажатии на стрелку у родителя(<li>)появляется класс _active или исчезает,если он был!!!
-    arrArrows.forEach(el => el.addEventListener('click', function () {
-        el.parentElement.classList.toggle('_active');
-    }));
-
+    if (arrArrows.length > 0) {
+        arrArrows.forEach(el => el.addEventListener('click', function () {
+            el.parentElement.classList.toggle('_active');
+        }));
+    }
 } else {
     document.body.classList.add('_pc');
+}
+
+// механизм smooth scrolling to sections
+//массив всех элементов, у которых есть атрибут data-goto
+const dataGo = Array.from(document.querySelectorAll('.menu__link[data-goto]'));
+if (dataGo.length > 0) {
+    dataGo.forEach(el => {
+        el.addEventListener('click', clickMenuLink);
+    });
+
+    function clickMenuLink(e) {
+        //обязательно,чтобы отключить перезагрузку страницы,которую совершает по умолчанию элемент ссылка
+        e.preventDefault();
+        //элемент на который мы совершили клик
+        const menuLink = e.target;
+        //получаем значение из data attribute
+        const classEl = menuLink.dataset.goto;
+        //get element by class name
+        const gotoBlock = document.querySelector(classEl);
+        console.log(gotoBlock);
+        if (classEl && gotoBlock) {
+            //pageYOffset - это прокрутка документа,величина текущего скролла
+            const heightHeader = document.querySelector('.header').offsetHeight;
+            //
+            const valueGotoBlock = gotoBlock.getBoundingClientRect().top + pageYOffset - heightHeader;
+            console.log('height', valueGotoBlock);
+            //это код,который позволяет скролить к нужному элементу
+            window.scrollTo({
+                top: valueGotoBlock,
+                behavior: "smooth",
+            });
+            // valueGotoBlock=0;
+        }
+    }
 }
