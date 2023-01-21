33bdf57e8e61a714c74b544c101f6d04f87eee8e
Lelik Jan <21alex7777@gmail.com>
2023-01-21 18:20:38 +0300

    feat: add styles and js for icon animation

--- src/index.js
+++ src/index.js
@@ -41,6 +41,21 @@ if (isMobile.any()) {
     document.body.classList.add('_pc');
 }
 
+//mechanism open and close menu on mobile
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (menuIcon) {
+    menuIcon.addEventListener('click', function () {
+        // это закрытие и открытие меню - добавляет или удаляет класс _active
+        menuBody.classList.toggle('_active');
+        //это анимация иконки -добавляет или удаляет класс _active
+        menuIcon.classList.toggle('_active');
+        //запрещаем скролл страницы в мобильном режиме при открытом меню - добавляет или удаляет класс _lock
+        document.body.classList.toggle('_lock');
+    })
+}
+
+
 // механизм smooth scrolling to sections
 //массив всех элементов, у которых есть атрибут data-goto
 const dataGo = Array.from(document.querySelectorAll('.menu__link[data-goto]'));
@@ -65,12 +80,18 @@ if (dataGo.length > 0) {
             //
             const valueGotoBlock = gotoBlock.getBoundingClientRect().top + pageYOffset - heightHeader;
             console.log('height', valueGotoBlock);
+//проверка на класс _active,который для menu__body смещает его влево,для menu__icon превращает иконку в крестик
+            if(menuIcon.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                menuBody.classList.remove('_active');
+                menuIcon.classList.remove('_active');
+            }
             //это код,который позволяет скролить к нужному элементу
             window.scrollTo({
                 top: valueGotoBlock,
                 behavior: "smooth",
             });
-            // valueGotoBlock=0;
+
         }
     }
 }
